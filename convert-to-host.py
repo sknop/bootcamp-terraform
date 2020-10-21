593f8f827435578cbacd0454b37435ee90ba7405
Sven Erik Knop <seknop@gmail.com>
2020-10-21 16:59:14 +0100

    First version of converter working

---
+++ convert-to-host.py
@@ -0,0 +1,83 @@
+from __future__ import print_function
+
+from jinja2 import Template
+import json
+import sys
+import pprint
+
+OUTPUT_KEYS = {
+    'kafka_broker' : 'broker_private_dns',
+    'kafka_connect' : 'connect_private_dns',
+    'schema_registry' : 'schema_private_dns',
+    'control_center' : 'control_center_private_dns',
+    'ksql' : 'ksql_private_dns',
+    'kafka_rest' : 'rest_private_dns',
+    'zookeeper' : 'zookeeper_private_dns'
+}
+
+CLUSTER_DATA = 'cluster_data'
+
+class TerraformResults:
+    def __init__(self, fname, uname, tempFile):
+        self.filename = fname
+        self.username = uname
+        self.template = self.create_template(tempFile)
+
+        self.json_output = self.parse_json()
+        self.all_ips = []
+        self.ip_dict = {}
+
+        self.filter_json()
+
+    def create_template(self, tempFile):
+        with open(tempFile) as f:
+            temp = f.read()
+
+        return Template(temp)
+
+    def parse_json(self):
+        with open(self.filename) as f:
+            content = f.read()
+
+        output = json.loads(content)
+        return output
+
+    def filter_json(self):
+        for key,name in OUTPUT_KEYS.items():
+            ip = self.filter_item(name)[0]
+            self.all_ips += ip
+            self.ip_dict[key] = ip
+
+        self.ip_dict[CLUSTER_DATA] = self.json_output[CLUSTER_DATA]['value']
+
+    def filter_item(self, name):
+        return self.json_output[name]['value']
+
+    def output(self):
+        pp = pprint.PrettyPrinter(indent=4)
+
+        print('All IPs:')
+        pp.pprint(self.all_ips)
+
+        print()
+
+        print('Broking into sections:')
+        pp.pprint(self.ip_dict)
+
+        self.print_ip()
+        self.print_hosts()
+
+    def print_ip(self):
+        with open(self.username + '.txt', 'w+') as f:
+            f.writelines('\n'.join(self.all_ips))
+
+    def print_hosts(self):
+        print(self.template.render(self.ip_dict))
+
+if __name__ == '__main__':
+    filename = sys.argv[1]
+    username = sys.argv[2]
+    template = sys.argv[3]
+
+    results = TerraformResults(filename, username, template)
+    results.output()
