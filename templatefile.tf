99dc7f1746dc9a7b94fd5554fed6d9f4937f8148
Sven Erik Knop <seknop@gmail.com>
2023-03-27 13:52:42 +0100

    Automatically add broker.rack to each broker node.

--- templatefile.tf
+++ templatefile.tf
@@ -10,7 +10,8 @@ resource "local_file" "ansible_inventory" {
   content = templatefile("${path.module}/${var.hosts_templatefile}",
     {
       zookeepers = aws_instance.zookeepers.*.private_dns
-      kafka_brokers = aws_instance.brokers.*.private_dns
+      kafka_brokers =  aws_instance.brokers.*.private_dns
+      kafka_broker_azs =  aws_instance.brokers.*.availability_zone
       kafka_connects = aws_instance.connect-cluster.*.private_dns
       ksqls = aws_instance.ksql.*.private_dns
       kafka_rests = aws_instance.rest.*.private_dns
